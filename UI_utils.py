d7315d4f7f34214f5ec0ca10a7d36b381befeef4
nadinedronia <nadine.dronia@outlook.de>
2025-05-18 15:36:29 +0200

    UI_Utils_1_fertig

--- UI_utils.py
+++ UI_utils.py
@@ -1,26 +1,25 @@
-import os          # Für Datei- und Pfadprüfung (z. B. ob config.toml existiert)
-import toml        # Für das Lesen und Schreiben von TOML-Dateien
-
+"""
+@file UI_utils.py
+@brief lädt eine TOML-Konfigurationsdatei und stellt sie als Dictionary bereit.
+"""
+import os          
+#Für Datei- und Pfadprüfung (z. B. ob config.toml existiert)
+import toml        
+#Modul zum Parsen von .toml-Dateien
 
 #: @var KONFIGURATIONSDATEI
 #: @brief Pfad zur standardmäßigen Konfigurationsdatei, wird großgeschrieben da Konstante 
 KONFIGURATIONSDATEI = "config.toml"
 
-# Definiert eine Funktion zum Laden der Konfigurationsdatei
-# Gibt den Inhalt als Python-Dictionary zurück
-
 def lade_config():
-    # Prüft, ob die Datei config.toml im aktuellen Verzeichnis existiert
-    if os.path.exists(KONFIGURATIONSDATEI):
-      
-        """
-    Lädt die TOML-Konfigurationsdatei und gibt sie als Dictionary zurück
+    """
+    @brief lädt die TOML-Konfigurationsdatei und gibt sie als Dictionary zurück (Parsing).
 
-    @brief Liest die Datei 'config.toml' ein und parst sie
     @return dict mit den Konfigurationswerten
     @raises FileNotFoundError wenn die Datei nicht existiert
     """
+    if os.path.exists(KONFIGURATIONSDATEI):
+       
         return toml.load(KONFIGURATIONSDATEI)
     else:
-        # Falls die Datei nicht existiert wird ein Fehler ausgelöst
-        raise FileNotFoundError("config.toml nicht gefunden.")
+        raise FileNotFoundError("config.toml nicht gefunden.") 

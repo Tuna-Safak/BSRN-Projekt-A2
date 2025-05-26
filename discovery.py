d0299a15c3defed1f801071be5f537d5508c985e
mozdaaz <moshdazami@hotmail.de>
2025-05-26 17:05:43 +0200

    funktionen update

--- discovery.py
+++ discovery.py
@@ -70,63 +70,64 @@ def sende_who():  # ✳️ NEU
     print("[SEND] → WHO gesendet")  # ✳️ NEU
 
 
-
+def nutzerspeichern(): 
 ## bindet den Socket an eine IP-Adresse und einen Port
 ## sock	= Variable, die den UDP-Socket enthält
 ## bind()	= Methode, um dem Socket eine Adresse (IP + Port) zu geben
 ## ('', DISCOVERY_PORT) = '' bedeutet: alle lokalen IP-Adressen, Port kommt aus der Konfigurationsdatei
 
-sock.bind(('', DISCOVERY_PORT))
-print(f"Discovery-Dienst läuft und hört auf {DISCOVERY_PORT}... ")
+  sock.bind(('', DISCOVERY_PORT))
+  print(f"Discovery-Dienst läuft und hört auf {DISCOVERY_PORT}... ")
 
-while True:
+
+  while True:
       ## recvfrom() = Methode für UDP-Sockets, liefert zwei Werte zurueck, 
       ## nachricht = in bytes (inhalt der nachricht), absender = IP-Adresse und Port des Absenders
       ## empfängt bis zu 1024 Bytes, Max. Nachrichtenlänge: 512 Zeichen, 1024 = doppelte Menge (Sicherheitsreserve)
-      nachricht, absender = sock.recvfrom(1024)
+        nachricht, absender = sock.recvfrom(1024)
 
       ## message = enthält die Nachricht als Bytes
       ## .decode() = wandelt Bytes in String um
       ## strip() = entfernt leerzeichen und zeilechumbrueche, vorne und hinten
-      message = nachricht.decode().strip()
+        message = nachricht.decode().strip()
 
       ## ueberpruefung 
-      if len(nachricht) > 512:
-        print("Nachricht zu lang - Verstoß gegen SLCP-Protokoll!")
-        continue
+        if len(nachricht) > 512:
+          print("Nachricht zu lang - Verstoß gegen SLCP-Protokoll!")
+          continue
       
       ## nachrichtTeilen = datentyp: list 
       ## split() = teilt die nachricht in Teilstrings 
-      nachrichtTeilen = message.split()
+        nachrichtTeilen = message.split()
       ## SLCP-Regel: Nachricht muss mindestens 1 Teil enthalten
       ## wenn Liste leer ist, dann überspringe den Rest der Schleife
       ## und mache mit nächster empfangenen Nachricht weiter
-      if not nachrichtTeilen:
-        continue
+        if not nachrichtTeilen:
+          continue
       
       ## erstes Wort aus der Nachricht holen, also den Befehl, den der Absender geschickt hat
-      befehl = nachrichtTeilen[0]
+        befehl = nachrichtTeilen[0]
 
-      if befehl == "JOIN" and len(nachrichtTeilen) == 3:
+        if befehl == "JOIN" and len(nachrichtTeilen) == 3:
        ## handle = benutzername
-        handle = nachrichtTeilen[1]
-        port = nachrichtTeilen[2]
+          handle = nachrichtTeilen[1]
+          port = nachrichtTeilen[2]
 
        ## IP-Adresse aus dem Datenpaket "absender" holen
-        ip = absender[0]   
+          ip = absender[0]   
 
        ## benutzer im Woerterbuch speichern
-        known_users[handle] = (ip, port)
+          known_users[handle] = (ip, port)
 
-        print(f" [INFO: ] {handle} ist jetzt bekannt unter {ip}: {port}")
+          print(f" [INFO: ] {handle} ist jetzt bekannt unter {ip}: {port}")
        ## who-block
 
-      elif befehl == "WHO" and len(nachrichtTeilen) == 1:
-         antwort = "KNOWNUSERS "
+        elif befehl == "WHO" and len(nachrichtTeilen) == 1:
+          antwort = "KNOWNUSERS "
 
          ## join() = alle Elemente in der Liste verbinden zu einem Teilstring 
          ## items() =  liste von Tupeln mit Schlüssel und Wert
-         antwort += ", ".join(
+          antwort += ", ".join(
             f"{handle} {ip} {port}" for handle, (ip, port) in known_users.items()
          )
 
@@ -134,16 +135,19 @@ while True:
          ## encode() = wandelt string in bytes um
          ## absender = IP-Adresse und Port an dem die Nachricht gehen soll
 
-         sock.sendto(antwort.encode(), absender)
+          sock.sendto(antwort.encode(), absender)
 
-         print(f"[SEND] → {absender}: {antwort}")
+          print(f"[SEND] → {absender}: {antwort}")
 
-      elif befehl == "LEAVE" and len(nachrichtTeilen) == 2:
-         handle = nachrichtTeilen[1]
+        elif befehl == "LEAVE" and len(nachrichtTeilen) == 2:
+          handle = nachrichtTeilen[1]
          
-      if handle in known_users:
+        if handle in known_users:
          
-            del known_users[handle]
-            print(f"[INFO] {handle} wurde entfernt (LEAVE empfangen)")
-      else:
-            print(f"[WARNUNG] LEAVE empfangen, aber {handle} war nicht bekannt")
+              del known_users[handle]
+              print(f"[INFO] {handle} wurde entfernt (LEAVE empfangen)")
+        else:
+             print(f"[WARNUNG] LEAVE empfangen, aber {handle} war nicht bekannt")
+
+
+     

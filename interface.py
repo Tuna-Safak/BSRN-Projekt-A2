b2493acff79d845639a98cd29d15534baf11efdf
JanaWie <Jana.Wiesinger@web.de>
2025-06-17 13:37:21 +0200

    mehtoden gelöscht

--- interface.py
+++ interface.py
@@ -87,13 +87,6 @@ def eingabe_bild():
 # @return Die Config-Datei wird verändert
 #gibt erst die aktuelle nachricht aus und fragt dann nach einer neuen
 #nachricht in der Config einsehbar
-'''def autoreply_aktivieren(config,pfad):
-    aktuell = config.get("client", {}).get("autoreply_aktiv", "")
-    print(f"Aktueller Status: {aktuell}")
-    if aktuell=="false":
-        config["client"]["autoreply_aktiv"] =true
-    else:
-        config["client"]["autoreply_aktiv"] =false'''
 
 def autoreply_umschalten(config, pfad):
     aktuell = config.get("client", {}).get("autoreply", "")  # Liest aktuellen Autoreply
@@ -123,26 +116,3 @@ def autoreply_einschalten(config, pfad):
 
     return config
 
-''' autoreply_umschalten(config, pfad):
-    aktuell = config.get("client", {}).get("autoreply", "")  # Liest aktuellen Autoreply
-    print(f"Aktueller Autoreply-Text: '{aktuell}'")
-    
-    neu = input("Neuer Autoreply-Text (leer für deaktivieren): ")
-    config["client"]["autoreply"] = neu
-    config["client"]["autoreply_aktiv"] = bool(neu.strip())  # Aktiv, wenn Text vorhanden
-
-    # >>> Änderung hier: benutze den tatsächlich geladenen Pfad statt festem Dateinamen
-    with open(pfad, "w") as f:
-        toml.dump(config, f)
-
-    return config'''
-'''def autoreply_umschalten(config, handle):
-    aktuell = config.get("client", {}).get("autoreply", "")#ließt den Wert von autoreply aus. Wenn er nicht vorhalen ist, kommt ein leerer
-    print(f"Aktueller Autoreply-Text: '{aktuell}'")
-    neu = input("Neuer Autoreply-Text (leer für deaktivieren): ")
-    config["client"]["autoreply"] = neu #greift auf die zeile autoreply in der config zu und ändert sie
-    config["client"]["autoreply_aktiv"] = bool(neu.strip())  
-    pfad = f"Konfigurationsdateien/config_{handle}.toml"
-    with open(pfad, "w") as f:
-        toml.dump(config, f)
-    return config'''
